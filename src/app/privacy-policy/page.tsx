c9d9ac8c70c8410a77da46fe44d79bcb340482c9
Prashant2303 <prashant1997.official@gmail.com>
2026-05-05 23:08:08 +0530

    Made privacy policy page client rendered

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,15 +1,26 @@
+'use client'
+
 import { db } from "@/firebase-config";
 import { collection, getDocs, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-export default async function PrivacyPolicy() {
-    const q = query(collection(db, "policy"));
-    const querySnapshot = await getDocs(q);
-    let __html = '';
-    querySnapshot.forEach((doc) => {
-        __html = doc.data().detail;
-    });
+export default function PrivacyPolicy() {
+    const [policyText, setPolicyText] = useState('');
+
+    useEffect(() => {
+        async function setPolicy() {
+            const q = query(collection(db, "policy"));
+            const querySnapshot = await getDocs(q);
+            let html = '';
+            querySnapshot.forEach((doc) => {
+                html = doc.data().detail;
+            });
+            setPolicyText(html);
+        }
+        setPolicy();
+    }, []);
 
     return <div>
-        <div dangerouslySetInnerHTML={{ __html }}></div>
+        <div dangerouslySetInnerHTML={{ __html: policyText }}></div>
     </div>
 }
